46eccfb3dc757caf5838a8136ecfeb217b5ac26b
wyx8267 <369264448@qq.com>
2020-08-30 23:02:24 +0800

    provide和inject实现aside切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>
+      <aside v-if="asideVisible">
         <h2>组件列表</h2>
         <ol>
           <li><router-link to="/doc/switch">Switch 组件</router-link></li>
@@ -17,9 +17,14 @@
 </template>
 <script lang="ts">
   import Topnav from '../components/Topnav.vue'
+  import { inject, Ref } from 'vue';
   export default {
     components: {
       Topnav
+    },
+    setup(){
+      const asideVisible = inject<Ref<boolean>>('asideVisible')
+      return {asideVisible}
     }
   };
 </script>
