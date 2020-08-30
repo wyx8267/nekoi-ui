21e9d4dd5eccafb912ba5645e6f6e58c28aa1dd8
wyx8267 <369264448@qq.com>
2020-08-30 23:14:10 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="topnav">
-      <div class="logo" @click="toggleMenu">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <span class="toggle-aside" @click="toggleMenu"></span>
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
@@ -28,6 +29,8 @@ export default {
     padding: 16px;
     position: relative;
     z-index: 10;
+    justify-content: center;
+    align-items: center;
     > .logo {
       max-width: 6em;
       margin-right: auto;
@@ -39,6 +42,21 @@ export default {
       > li {
         margin: 0 1em;
       }
+    }
+    > .toggle-aside {
+      width: 24px;
+      height: 24px;
+      background: red;
+      position: absolute;
+      left: 16px;
+      top: 50%;
+      transform: translateY(-50%);
+      display: none;
+    }
+    @media(max-width: 500px) {
+      > .menu {display: none;}
+      > .logo {margin: 0 auto;}
+      > .toggle-aside {display: inline-block;}
     }
   }
 </style>
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -5,10 +5,18 @@
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
         <ol>
-          <li><router-link to="/doc/switch">Switch 组件</router-link></li>
-          <li><router-link to="/doc/button">Button 组件</router-link></li>
-          <li><router-link to="/doc/dialog">Dialog 组件</router-link></li>
-          <li><router-link to="/doc/tabs">Tabs 组件</router-link></li>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
         </ol>
       </aside>
       <main>主内容</main>
@@ -16,27 +24,23 @@
   </div>
 </template>
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue'
-  import { inject, Ref } from 'vue';
-  export default {
-    components: {
-      Topnav
-    },
-    setup(){
-      const asideVisible = inject<Ref<boolean>>('asideVisible')
-      return {asideVisible}
-    }
-  };
+import Topnav from "../components/Topnav.vue";
+import { inject, Ref } from "vue";
+export default {
+  components: {
+    Topnav,
+  },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    return { asideVisible };
+  },
+};
 </script>
 <style lang="scss" scoped>
 aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  padding-top: 70px;
-  top: 0;
-  left: 0;
   > h2 {
     margin-bottom: 4px;
   }
@@ -44,6 +48,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    padding-top: 70px;
+    top: 0;
+    left: 0;
   }
 }
 </style>
