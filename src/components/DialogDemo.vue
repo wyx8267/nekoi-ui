0a507a88f8dce9891b89607526d66111411c607d
wyx8267 <369264448@qq.com>
2020-09-19 15:29:53 +0800

    Dialog组件创建

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    Dialog Demo
-  </div>
+  <div>Dialog Demo</div>
+  <Dialog></Dialog>
 </template>
+
+<script lang="ts">
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {
+    Dialog
+  }
+}
+</script>
