9aef0e05ac6749f4c741a6d7d9729755ce7c7737
wyx8267 <369264448@qq.com>
2020-10-18 11:34:35 +0800

    检查子组件类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -3,3 +3,18 @@
     Tabs 组件
   </div>
 </template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+  setup(props, context){
+    const defaults = context.slots.default()
+    defaults.forEach(tag=>{
+      if(tag.type !== Tab){
+        throw new Error("Tabs 子标签必须是 Tab");
+      }
+    })
+    return { defaults }
+  }
+}
+</script>
