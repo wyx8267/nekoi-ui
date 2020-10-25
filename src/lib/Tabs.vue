1d82edfd90e50728a9c5c7610c47a8f60f12c973
wyx8267 <369264448@qq.com>
2020-10-25 14:09:34 +0800

    渲染 Tabs 内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,20 +1,66 @@
 <template>
-  <div>
-    Tabs 组件
+  <div class="nekoi-tabs">
+    <div class="nekoi-tabs-nav">
+      <div
+        class="nekoi-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="nekoi-tabs-content">
+      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Tab from './Tab.vue'
+import Tab from "./Tab.vue";
 export default {
-  setup(props, context){
-    const defaults = context.slots.default()
-    defaults.forEach(tag=>{
-      if(tag.type !== Tab){
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
         throw new Error("Tabs 子标签必须是 Tab");
       }
-    })
-    return { defaults }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return { defaults, titles };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.nekoi-tabs{
+  &-nav{
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item{
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child{
+        margin-left: 0;
+      }
+
+      &.selected{
+        color: $blue;
+      }
+    }
+  }
+
+  &-content{
+    padding: 8px 0;
   }
 }
-</script>
+</style>
