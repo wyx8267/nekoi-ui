68b2c31e60a35bfa937ac1593bb1f33e99243b93
wyx8267 <369264448@qq.com>
2020-11-14 23:11:49 +0800

    首页样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,65 +1,82 @@
 <template>
   <div class="topnav">
-    <span class="toggle-aside" @click="toggleMenu"></span>
-    <div class="logo">LOGO</div>
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-neko"></use>
+      </svg>
+    </div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+      <li>
+        <router-link to="/doc">文档</router-link>
+      </li>
     </ul>
+    <span class="toggle-aside" @click="toggleMenu"></span>
   </div>
 </template>
 
 <script lang="ts">
-import { inject, Ref } from 'vue'
+import { inject, Ref } from "vue";
 export default {
-  setup(){
-    const asideVisible = inject<Ref<boolean>>('asideVisible')
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
     const toggleMenu = () => {
-      asideVisible.value = !asideVisible.value
-    }
-    return {toggleMenu, asideVisible}
-  }
-}
+      asideVisible.value = !asideVisible.value;
+    };
+    return { toggleMenu, asideVisible };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .topnav{
-    background: pink;
-    display: flex;
-    padding: 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    z-index: 10;
-    justify-content: center;
-    align-items: center;
-    > .logo {
-      max-width: 6em;
-      margin-right: auto;
-    }
-    > .menu {
-      display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-      > li {
-        margin: 0 1em;
-      }
-    }
-    > .toggle-aside {
-      width: 24px;
-      height: 24px;
-      background: red;
-      position: absolute;
-      left: 16px;
-      top: 50%;
-      transform: translateY(-50%);
-      display: none;
-    }
-    @media(max-width: 500px) {
-      > .menu {display: none;}
-      > .logo {margin: 0 auto;}
-      > .toggle-aside {display: inline-block;}
+$color: #007974;
+
+.topnav {
+  color: $color;
+  display: flex;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  justify-content: center;
+  align-items: center;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+    >svg{
+      width: 32px;
+      height: 32px;
     }
   }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+  > .toggle-aside {
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggle-aside {
+      display: inline-block;
+    }
+  }
+}
 </style>
