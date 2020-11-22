e9a76690bce147700ca584132840509adf693977
wyx8267 <369264448@qq.com>
2020-11-22 20:18:12 +0800

    展示源代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -3,43 +3,43 @@
     <h1>Switch 组件示例</h1>
     <div class="demo">
       <h2>常规用法</h2>
-      <div class="demo-compoent">
-        <Switch v-model:value="bool" />
+      <div class="demo-component">
+        <component :is="Switch1Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre>{{Switch1Demo.__sourceCode}}</pre>
       </div>
     </div>
     <div class="demo">
       <h2>支持 disabled</h2>
       <div class="demo-component">
-        <Switch v-model:value="bool" disabled />
+        <component :is="Switch2Demo" />
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+        <pre>{{Switch2Demo.__sourceCode}}</pre>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
 import Button from "../lib/Button.vue";
+import Switch1Demo from './Switch1.demo.vue'
+import Switch2Demo from './Switch2.demo.vue'
 import { ref } from "vue";
 export default {
   components: {
-    Switch,
-    Button
+    Button,
   },
   setup() {
     const bool = ref(false);
-    return { bool };
+    return { bool, Switch1Demo, Switch2Demo };
   },
 };
 </script>
